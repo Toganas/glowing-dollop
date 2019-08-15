839370f3a73820b8b502040ed255f7e2787339fc
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-08-15 02:58:34 -0500

    redirect when logged in

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,10 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { login } from "../../actions/auth";
 
-const Login = ({ login }) => {
+const Login = ({ login, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         email: "",
         password: "",
@@ -20,6 +20,10 @@ const Login = ({ login }) => {
 
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
+    // Redirect if logged in
+    if (isAuthenticated) {
+        return <Redirect to="/dashboard" />
+    }
     return (
         <Fragment>
             <h1 className="large text-primary">Sign In</h1>
@@ -56,6 +60,14 @@ const Login = ({ login }) => {
 
 Login.PropTypes = {
     login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
 }
 
-export default connect(null, { login })(Login);
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(
+    mapStateToProps,
+    { login })
+    (Login);
