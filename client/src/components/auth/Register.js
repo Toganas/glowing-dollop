ea10531e2bbe3cac014707b97a3227201d33da3f
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-09-11 11:39:49 -0500

    connecting the alerts to the state. Giving the alert a message and color

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,11 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from 'prop-types'
 
-const Register = () => {
+
+const Register = ({ setAlert }) => {
     const [formData, setFormData] = useState({
         name: "",
         email: "",
@@ -11,12 +15,13 @@ const Register = () => {
 
     const { name, email, password, password2 } = formData;
 
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const onChange = e =>
+        setFormData({ ...formData, [e.target.name]: e.target.value });
 
     const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
-            console.log("Passwords do not match");
+            setAlert("Passwords do not match", "danger");
         } else {
             console.log("SUCCESS");
         }
@@ -77,4 +82,11 @@ const Register = () => {
     )
 }
 
-export default Register
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
+};
+
+export default connect(
+    null,
+    { setAlert }
+)(Register);
